0bc7515c2143d8ef12cfe14cfa31a5d0f3798ed8
VaishakRenjith <vrenjith890@student.fuhsd.org>
2024-03-17 14:28:01 -0700

    added basic xp/currency script

---
+++ Assets/Scripts/LevelXPManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelXPManager : MonoBehaviour
+{
+    public int xp = 0;
+    public int currency = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("coin")) {
+            updateCurrency(10); //10 is a placeholder amt
+        } else if (other.CompareTag("xp")) {
+            updateXP(10); //10 is a placeholder amt
+        }
+    }
+
+    void updateXP(int amt)
+    {
+        xp += amt;
+    }
+
+    void updateCurrency(int amt)
+    {
+        currency += amt;
+    }
+}
