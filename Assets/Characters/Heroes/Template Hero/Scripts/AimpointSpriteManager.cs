d79c3734d182b84c91809dcb526547894ea0ae34
Jemian Lam <jlam178@student.fuhsd.org>
2024-03-17 15:24:32 -0700

    Added a buffer to sprite flipping, along with character movement scripts & anims

---
+++ Assets/Characters/Heroes/Template Hero/Scripts/AimpointSpriteManager.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class AimpointSpriteManager : MonoBehaviour
+{
+    //this script also handles the flipping of the player character
+    SpriteRenderer characterSprite;
+    Transform rotationPoint;
+    SpriteRenderer thisSprite;
+    int flipAngleBuffer;
+    float orderFlipRightThreshold;
+    float orderFlipLeftThreshold;
+    float bufferDistance;
+    void Start()
+    {
+        rotationPoint = GameObject.FindGameObjectWithTag("PlayerAimRotationPoint").GetComponent<Transform>();
+        thisSprite = GetComponent<SpriteRenderer>();
+        characterSprite = GameObject.FindGameObjectWithTag("Player").GetComponent<SpriteRenderer>();
+        orderFlipRightThreshold = 0;
+        orderFlipLeftThreshold = 180;
+        bufferDistance = 0.25f;
+    }
+    void Update()
+    {
+        SwitchSortingOrder();
+        FlipUpdate();
+    }
+
+    bool AimpointIsBehindPlayer() {
+        return rotationPoint.rotation.z > orderFlipRightThreshold && rotationPoint.rotation.z < orderFlipLeftThreshold;
+    }
+
+    void SwitchSortingOrder() {
+        if (AimpointIsBehindPlayer() && thisSprite.sortingLayerName != "BehindPlayer") {
+            thisSprite.sortingLayerName = "BehindPlayer";
+        } else if (thisSprite.sortingLayerName != "InFrontPlayer" && !AimpointIsBehindPlayer()) {
+            thisSprite.sortingLayerName = "InFrontPlayer";
+        }
+    }
+    public bool AimPointIsOnRightSide() { 
+        //return rotationPoint.rotation.z > -90 && rotationPoint.rotation.z < 90; //always returns true for some reason...
+        return thisSprite.transform.position.x > rotationPoint.position.x + bufferDistance;
+    }
+    void FlipUpdate() {
+        if (AimPointIsOnRightSide() && !IsWithinBuffer() && thisSprite.flipY) { //if on right and is flipped
+            thisSprite.flipY = false; //then unflip
+            characterSprite.flipX = false;
+        } else if (!AimPointIsOnRightSide() && !IsWithinBuffer() && !thisSprite.flipY) { //if on left and not already flipped
+            thisSprite.flipY = true;
+            characterSprite.flipX = true;
+        }
+    }
+    bool IsWithinBuffer() {
+        return Math.Abs(thisSprite.transform.position.x - rotationPoint.position.x) <= bufferDistance;
+    }
+}
---
+++ Assets/Characters/Heroes/Template Hero/Scripts/AnimController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimController : MonoBehaviour
+{
+    Animator pAnimator;
+    Rigidbody2D playerRb;
+
+    //anim names
+    const string MOVE = "Move";
+    const string IDLE = "Idle";
+    const string BACKMOVE = "BackwardsMove";
+
+    Transform prevPos;
+    Transform newPos;
+    AimpointSpriteManager aimpointRef;
+
+    void Start()
+    {
+        pAnimator = GetComponent<Animator>();
+        aimpointRef = GameObject.FindGameObjectWithTag("AimPointer").GetComponent<AimpointSpriteManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsIntentionallyMoving()) {
+            if (IsMovingForward())  {
+                pAnimator.Play(MOVE);
+            } else { //if doing the michael jackson
+                pAnimator.Play(BACKMOVE);
+            }
+        } else {
+            pAnimator.Play(IDLE);
+        }
+    }
+
+private void LateUpdate() {
+    prevPos = newPos;
+}
+
+    bool IsIntentionallyMoving() { //in case the player will get pushed around.
+        return transform.hasChanged && (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0);
+    }
+
+    bool IsMovingForward() {
+        return aimpointRef.AimPointIsOnRightSide() && Input.GetAxisRaw("Horizontal") > 0 ||
+        !aimpointRef.AimPointIsOnRightSide() && Input.GetAxisRaw("Horizontal") < 0 ||
+        IsOnlyMovingVertically();
+    }
+
+    bool IsOnlyMovingVertically() {
+        return Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") != 0;
+    }
+}
