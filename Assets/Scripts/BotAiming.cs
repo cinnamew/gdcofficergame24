73accd232bc386b6037259fa56984e484774fb2f
cuberperson <cuber.person123@gmail.com>
2024-03-16 18:55:28 -0700

    Added Enemy Aiming with the cability of Decoy Objects

--- Assets/Scripts/BotAiming.cs
+++ Assets/Scripts/BotAiming.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class BotAiming : MonoBehaviour
 {
-    Vector2 aimPoint; // players aim at mouse, enemies with projectiles aim at players, AI buddies aim at enemies
+    public Vector2 aimPoint; // players aim at mouse, enemies with projectiles aim at players, AI buddies aim at enemies
     //AI should have a target priority (e.g. enemies will go for a bait object if a player uses one)
-    int aimPriority;
+    [SerializeField] public int aimPriority;
+    [SerializeField] private bool enableAim = true;
     //e.g. a player will have an aimPriority of 1 but the cherry bomb he deploys will have an aimPriority of 2
     // the enemies will go for whatever object has the higher aimPriority value
     private void Update() {
-        //every frame, look at the aimpoint
+        if (enableAim){
+            //every frame, look at the aimpoint
+            CheckAimPriority();
+            Vector2 direction = aimPoint - (Vector2)transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = rotation;
+        }
     }
-    void CheckAimPriority() {
+    public virtual void CheckAimPriority() {
         //first check for whatever target is closest. Then check aimPriority value
     }
     //consider detection range
