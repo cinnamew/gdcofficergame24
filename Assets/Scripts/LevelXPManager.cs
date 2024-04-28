b7e08d8e015f35d95f8aac242fc8d2d44a5fd376
VaishakRenjith <vrenjith890@student.fuhsd.org>
2024-04-28 11:07:29 -0700

    rewrote xp script, UI implementation needs to be scripted

--- Assets/Scripts/LevelXPManager.cs
+++ Assets/Scripts/LevelXPManager.cs
@@ -1,51 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class LevelXPManager : MonoBehaviour
 {
     public int xp = 0;
-    private int maxEXP = 15;
+    private int xpForLevelUp = 79;
     private int level = 1;
-    public int currency = 0;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("coin")) {
-            updateCurrency(10); //10 is a placeholder amt
-        } else if (other.CompareTag("xp")) {
-            updateXP(10); //10 is a placeholder amt
+        if (other.CompareTag("xp")) 
+        { 
+            System.Random r = new System.Random();
+            switch (other.gameObject.GetComponent<SpriteRenderer>().sprite.name)
+            {
+                case "xpBlue":
+                    updateXP(r.Next(1, 11)); // (inclusive, exclusive)
+                    break;   
+                case "xpGreen":
+                    updateXP(r.Next(11, 20)); // (inclusive, exclusive)
+                    break;
+                case "xpYellow":
+                    updateXP(r.Next(20, 50)); // (inclusive, exclusive)
+                    break;
+                case "xpOrange":
+                    updateXP(r.Next(50, 100)); // (inclusive, exclusive)
+                    break;
+                case "xpPurple":
+                    updateXP(r.Next(100, 200)); // (inclusive, exclusive)
+                    break;
+                case "xpRed":
+                    updateXP(r.Next(200, 400)); // (inclusive, exclusive)
+                    break;
+
+            }
         }
     }
-
-    public void updateXP(int amt)
+    
+    public void updateXP(int xpGained) 
     {
-        xp += amt;
-        updateLevel();
-    }
-
-    private void updateLevel()
-    {
-        if (xp >= maxEXP){
-            level += 1;
-            maxEXP = maxEXP * 2;
+        xp += xpGained;
+        if (xp >= xpForLevelUp)
+        {
+            xpForLevelUp = Convert.ToInt32(4 * Math.Pow(level+1, 2.1)) - Convert.ToInt32(Math.Pow(4 * level, 2.1));
+            level++;
         }
-    }
-
-    public void updateCurrency(int amt)
-    {
-        currency += amt;
     }
 }
