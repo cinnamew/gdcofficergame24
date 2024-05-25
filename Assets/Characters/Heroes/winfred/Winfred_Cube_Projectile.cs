1b0927dcf885a4f4902c18bba94be57fbac0ac63
Faye-Yu <fayeyu0103@gmail.com>
2024-05-25 15:41:41 -0700

    some attacks stuff

--- Assets/Characters/Heroes/winfred/Winfred_Cube_Projectile.cs
+++ Assets/Characters/Heroes/winfred/Winfred_Cube_Projectile.cs
@@ -2,16 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cube_Projectile_VFX : MonoBehaviour
+public class Winfred_Cube_Projectile : MonoBehaviour
 {
     Rigidbody2D rb;
     Vector2 dir;
     ParticleSystem ps;
+
+    Vector2 startingPosition;
+    private float theta; //in radians
+    private float speed;
+    private float startAngle; //in radians
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         ps = GetComponentInChildren<ParticleSystem>();
+
+        startingPosition = transform.position;
+        speed = rb.velocity.magnitude;
+        startAngle = Mathf.Atan2(rb.velocity.y, rb.velocity.x);
+
+        theta = -Mathf.PI / 8;
     }
 
     // Update is called once per frame
@@ -27,8 +38,15 @@ public class Cube_Projectile_VFX : MonoBehaviour
             }
             angle -= ps.shape.arc / 2;
             angle += 180;
-            //Debug.Log(angle);
             ps.gameObject.transform.eulerAngles = new Vector3(0, 0, angle-90);
         }
+
+        Vector2 r = (Vector2)transform.position - startingPosition;
+        theta += Time.deltaTime;
+        
+        float x = theta;
+        float dx = Mathf.Sin(x) * (-Mathf.Cos(4*x)) - 4 * Mathf.Sin(4*x) * Mathf.Cos(x);
+        float dy = Mathf.Cos(x) * Mathf.Cos(4 * x) - 4 * Mathf.Sin(x) * (Mathf.Sin(4 * x));
+        rb.velocity = speed * new Vector2(dx, dy).normalized;
     }
 }
---
+++ Assets/Laurier_BusinessCard_Projectile.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laurier_BusinessCard_Projectile : MonoBehaviour
+{
+    [SerializeField] GameObject childProjectile;
+    private Rigidbody2D rb;
+    private List<GameObject> children = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        float originalAngle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg;
+
+        float angle = originalAngle + 120;
+        GameObject c1 = Instantiate(childProjectile, transform.position, Quaternion.identity);
+        c1.transform.eulerAngles = new Vector3(0, 0, angle);
+        c1.GetComponent<Rigidbody2D>().velocity = rb.velocity.magnitude * new Vector2(Mathf.Cos(angle* Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad));
+        children.Add(c1);
+        angle = originalAngle - 120;
+        GameObject c2 = Instantiate(childProjectile, transform.position, Quaternion.identity);
+        c2.transform.eulerAngles = new Vector3(0, 0, angle);
+        c2.GetComponent<Rigidbody2D>().velocity = rb.velocity.magnitude * new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad));
+        children.Add(c2);
+    }
+    private void OnDestroy()
+    {
+        foreach(GameObject o in children){
+            Destroy(o);
+        }
+    }
+}
