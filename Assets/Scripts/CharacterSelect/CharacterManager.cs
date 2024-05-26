6d8d2a8384b485a1803e16d1506a85ce83225ffb
VaishakRenjith <vrenjith890@student.fuhsd.org>
2024-05-26 15:09:42 -0700

    character selection

---
+++ Assets/Scripts/CharacterSelect/CharacterManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using Com.LuisPedroFonseca.ProCamera2D;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour
+{
+    public GameObject[] charaPrefabs;
+    public Vector3 spawnPos;
+    [SerializeField] private ProCamera2D cam;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //check if player prefs string is not null or empty
+        //convert playerprefs into chara prefab
+        //check if chara is null, error if there is no prefab assosciating the string
+        //if not null instantiate the character at the spawn point
+        string charaString = PlayerPrefs.GetString("SelectedCharacter");
+        if (!string.IsNullOrEmpty(charaString)) 
+        {
+            GameObject charaObject = getCharaByName(charaString);
+            if (charaObject != null) 
+            {
+                GameObject playerInScene = Instantiate(charaObject, spawnPos, Quaternion.identity);
+                cam.AddCameraTarget(playerInScene.transform);
+            }
+        }
+
+    }
+
+    GameObject getCharaByName(string nameAsString) 
+    {
+        foreach (GameObject g in charaPrefabs) 
+        {
+            if (nameAsString == g.name)
+                return g;
+        }
+        return null;
+    }
+}
