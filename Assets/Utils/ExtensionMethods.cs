b4bd1705f26516fcb1d64e2204713d29256bf600
cinnamew <101888513+cinnamew@users.noreply.github.com>
2024-05-27 12:01:46 -0700

    thief pt 2
    i stoled it

---
+++ Assets/Utils/ExtensionMethods.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public static class ExtensionMethods
+{
+    public static void RemoveCloneName(this GameObject gameObject) =>
+        gameObject.name = gameObject.name.Replace("(Clone)", string.Empty);
+
+    public static void ForEach<TKey, TValue>(this Dictionary<TKey, TValue> dict, Action<TKey, TValue> action)
+    {
+        foreach (var mType in dict)
+            action?.Invoke(mType.Key, mType.Value);
+    }
+
+    public static void ForEach<TKey, TValue>(this Dictionary<TKey, TValue> dict, Action<TValue> value)
+    {
+        foreach (var mType in dict)
+            value?.Invoke(mType.Value);
+    }
+
+    public static T GetOrAddComponent<T>(this GameObject tGO) where T : Component
+    {
+        if (null == tGO)
+        {
+            return null;
+        }
+
+        var type = typeof(T);
+        var component = tGO.GetComponent(type);
+        if (null == component)
+        {
+            component = tGO.AddComponent(type);
+        }
+
+        return component as T;
+    }
+
+    public static bool IsNullOrEmpty<T>(this T[] array) => array == null || array.Length < 1;
+
+    public static bool IsNullOrEmpty<T>(this List<T> list) => list == null || list.Count < 1;
+
+    public static bool IsNullOrEmpty<T>(this Queue<T> queue) => queue == null || queue.Count < 1;
+
+    public static bool IsNullOrEmpty<T1, T2>(this Dictionary<T1, T2> dictionary) =>
+        dictionary == null || dictionary.Count < 1;
+
+    public static bool IsNull(this GameObject go) => go == null;
+
+    public static bool IsNull(this Component component) => component == null;
+
+    public static T SelectRandom<T>(this T[] array)
+    {
+        return array.IsNullOrEmpty() ? default : array[UnityEngine.Random.Range(0, array.Length)];
+    }
+
+    public static T SelectRandom<T>(this List<T> list)
+    {
+        return list.IsNullOrEmpty() ? default : list[UnityEngine.Random.Range(0, list.Count)];
+    }
+
+    public static Vector2Int ToVector2Int(this Vector2 vector2) => new Vector2Int(Mathf.RoundToInt(vector2.x), Mathf.RoundToInt(vector2.y));
+
+    public static Vector2 ToVector2(this Vector2Int vector2Int) => new Vector2(vector2Int.x, vector2Int.y);
+    
+    public static Coroutine DoAfter(this MonoBehaviour monoBehaviour, float seconds, Action action) => monoBehaviour.StartCoroutine(DoAfterCoroutine(seconds, action));
+    
+    private static IEnumerator DoAfterCoroutine(float seconds, Action action)
+    {
+        yield return new WaitForSeconds(seconds);
+        action?.Invoke();
+    }
+    
+    public static void DoAfter(this MonoBehaviour monoBehaviour, Func<bool> condition, Action action) => monoBehaviour.StartCoroutine(DoAfterCoroutine(condition, action));
+    
+    private static IEnumerator DoAfterCoroutine(Func<bool> condition, Action action)
+    {
+        yield return new WaitUntil(condition);
+        action?.Invoke();
+    }
+
+    public static void FadeIn(this CanvasGroup canvasGroup, float duration)
+    {
+        canvasGroup.DOFade(1f, duration).OnComplete(() =>
+        {
+            canvasGroup.interactable = true;
+            canvasGroup.blocksRaycasts = true;
+        });
+    }
+    
+    public static void FadeOut(this CanvasGroup canvasGroup, float duration)
+    {
+        canvasGroup.DOFade(0f, duration).OnComplete(() =>
+        {
+            canvasGroup.interactable = false;
+            canvasGroup.blocksRaycasts = false;
+        });
+    }
+}
---
+++ Assets/Utils/SaveManager.cs
@@ -0,0 +1,127 @@
+﻿using System.IO;
+using UnityEngine;
+
+/// <summary>
+/// Use this class to save data.
+/// </summary>
+public static class SaveManager
+{
+    #region PlayerPrefs
+    
+    public static void SaveInt(string key, int value) => PlayerPrefs.SetInt(key, value);
+    
+    public static int LoadInt(string key, int defaultValue = 0) => PlayerPrefs.GetInt(key, defaultValue);
+    
+    public static void SaveFloat(string key, float value) => PlayerPrefs.SetFloat(key, value);
+    
+    public static float LoadFloat(string key, float defaultValue = 0f) => PlayerPrefs.GetFloat(key, defaultValue);
+    
+    public static void SaveString(string key, string value) => PlayerPrefs.SetString(key, value);
+    
+    public static string LoadString(string key, string defaultValue) => PlayerPrefs.GetString(key, defaultValue);
+
+    public static void Save(string key, object data)
+    {
+        var json = JsonUtility.ToJson(data);
+        PlayerPrefs.SetString(key, json);
+    }
+    
+    public static T Load<T>(string key)
+    {
+        var json = PlayerPrefs.GetString(key);
+        return JsonUtility.FromJson<T>(json);
+    }
+    
+    public static bool HasKey(string key) => PlayerPrefs.HasKey(key);
+
+    #endregion
+
+    #region File-based
+    
+    private const string SaveFileExtension = ".json";
+
+    private static string _mainSavePath = Application.persistentDataPath;
+
+    public static void Save(string folderName, string fileName, object data)
+    {
+        CheckOrCreateDirectory(folderName);
+        
+        var json = JsonUtility.ToJson(data);
+        File.WriteAllText(_mainSavePath + $"/{folderName}/" + fileName + SaveFileExtension, json);
+        
+        Debug.Log(Green($"Saved {fileName} to {folderName}"));
+    }
+    
+    public static T Load<T>(string folderName, string fileName)
+    {
+        if (!CheckFolder(folderName)) return default;
+        
+        var json = File.ReadAllText(_mainSavePath + $"/{folderName}/" + fileName + SaveFileExtension);
+        var obj = JsonUtility.FromJson<T>(json);
+        
+        Debug.Log(Green($"Loaded {fileName} from {folderName}"));
+        
+        return obj;
+    }
+
+    public static T[] LoadAllFromDirectory<T>(string folderName)
+    {
+        CheckOrCreateDirectory(folderName);
+        var files = Directory.GetFiles(_mainSavePath + $"/{folderName}/");
+        var data = new T[files.Length];
+        for (var i = 0; i < files.Length; i++)
+        {
+            var json = File.ReadAllText(files[i]);
+            data[i] = JsonUtility.FromJson<T>(json);
+        }
+        
+        Debug.Log(Green($"Loaded all files from {folderName}"));
+
+        return data;
+    }
+    
+    public static (string[] names, T[] objects) LoadAllFromDirectoryWithNames<T>(string subPath)
+    {
+        CheckOrCreateDirectory(subPath);
+        var files = Directory.GetFiles(_mainSavePath + $"/{subPath}/");
+
+        var names = new string[files.Length];
+        var data = new T[files.Length];
+        for (var i = 0; i < files.Length; i++)
+        {
+            names[i] = Path.GetFileNameWithoutExtension(files[i]);
+            
+            var json = File.ReadAllText(files[i]);
+            data[i] = JsonUtility.FromJson<T>(json);
+        }
+        
+        Debug.Log(Green($"Loaded all files from {subPath}"));
+        
+        return (names, data);
+    }
+
+    private static void CheckOrCreateDirectory(string folderName)
+    {
+        if (Directory.Exists(_mainSavePath + $"/{folderName}/")) return;
+        
+        Debug.LogWarning($"New directory created: {_mainSavePath}/{folderName}/");
+        Directory.CreateDirectory(_mainSavePath + $"/{folderName}/");
+    }
+    
+    private static bool CheckPath(string path)
+    {
+        if (Directory.Exists(Path.GetDirectoryName(path))) return true;
+        
+        Debug.LogError("Directory does not exist");
+        return false;
+    }
+
+    private static bool CheckFolder(string subPath) => CheckPath(_mainSavePath + $"/{subPath}/");
+    
+    /// <summary>
+    /// Sets the color of the text to green (via rich text)
+    /// </summary>
+    private static string Green(string text) => $"<b><color=green>{text}</color></b>";
+
+    #endregion
+}
