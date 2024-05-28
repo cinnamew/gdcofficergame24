10ced3845f5791e81734b63db06be9c61b840994
cuberperson <cuber.person123@gmail.com>
2024-05-27 22:58:14 -0700

    pew pew (Untested)
    Added Fair and balanced upgrade for Jemi (Untested)

--- Assets/Scripts/WeaponManager.cs
+++ Assets/Scripts/WeaponManager.cs
@@ -13,11 +13,31 @@ public class WeaponManager : MonoBehaviour
 
     // Update is called once per frame
     public void Upgrade(UpgradeItem upgrade){
+        if (gameObject.name.Contains("Arnav")){
+            //Do something
+        }
+        if (gameObject.name.Contains("Faye")){
+            //Do something
+        }
+        if (gameObject.name.Contains("Jemi")){
+            //Do something
+        }
         if (gameObject.name.Contains("Jolie")){
             gameObject.GetComponent<OrbAttack>().upgradeOrbs();
-        } else if (gameObject.name.Contains("Jemi")){
-            gameObject.GetComponent<OrbAttack>().upgradeOrbs();
-        } else if (gameObject.name.Contains("Winfred")){
+        }
+        if (gameObject.name.Contains("Laurier")){
+            //Do something
+        }
+        if (gameObject.name.Contains("Lydia")){
+            //Do something
+        }
+        if (gameObject.name.Contains("Rohan")){
+            //Do something
+        }
+        if (gameObject.name.Contains("Vaishak")){
+            //Do something
+        }
+        if (gameObject.name.Contains("Winfred")){
             if (upgrade.currentUpgradeLvl == 1){
                 gameObject.GetComponent<PlayerAttack>().SetBaseProjectileCooldown(4f);
             }
